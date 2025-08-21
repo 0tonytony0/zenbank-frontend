7156bc64a983d07f6150c32c5298fe76f7099e45
Nikhil Verma <nikhilvermaa036@gmail.com>
2025-08-21 18:11:11 +0530

    adding dropdown

--- src/components/Personal/PersonalBenefits.jsx
+++ src/components/Personal/PersonalBenefits.jsx
@@ -1,44 +1,44 @@
-// import { div } from "framer-motion/client";
-// import React from "react";
-// import './banner.css'
-// import { LuWalletMinimal } from "react-icons/lu";
-// import { RiHandCoinLine } from "react-icons/ri";
-// import { TbAntennaBars5 } from "react-icons/tb"; 
+import { div } from "framer-motion/client";
+import React from "react";
+import './banner.css'
+import { LuWalletMinimal } from "react-icons/lu";
+import { RiHandCoinLine } from "react-icons/ri";
+import { TbAntennaBars5 } from "react-icons/tb"; 
 
-// const personalBenefits = (props) => {
-//     return (
-//         <>
-//             <div className="benefitBox">
-//                 <div className="boxBenefits">
-//                     <div className="benefithead">
-//                         <h1>{props.Heading1}</h1>
-//                         <p>{props.Paragraph1}</p>
-//                     </div>
+const personalBenefits = (props) => {
+    return (
+        <>
+            <div className="benefitBox">
+                <div className="boxBenefits">
+                    <div className="benefithead">
+                        <h1>{props.Heading1}</h1>
+                        <p>{props.Paragraph1}</p>
+                    </div>
 
-//                     <div className="benefitsPoints">
+                    <div className="benefitsPoints">
 
-//                         <div className="benefitsCard">
-//                             <h3><RiHandCoinLine className="fawallet" size={40}/>{props.Heading2}</h3>
-//                             <p>{props.Paragraph2}</p>
-//                         </div>
+                        <div className="benefitsCard">
+                            <h3><RiHandCoinLine className="fawallet" size={40}/>{props.Heading2}</h3>
+                            <p>{props.Paragraph2}</p>
+                        </div>
 
-//                         <div className="benefitsCard">
-//                             <h3><LuWalletMinimal className="fawallet" size={40}/>{props.Heading3}</h3>
-//                             <p>{props.Paragraph3}</p>
-//                         </div>
+                        <div className="benefitsCard">
+                            <h3><LuWalletMinimal className="fawallet" size={40}/>{props.Heading3}</h3>
+                            <p>{props.Paragraph3}</p>
+                        </div>
 
-//                         <div className="benefitsCard">
-//                             <h3><TbAntennaBars5 className="fawallet " size={40}/>{props.Heading4}</h3>
-//                             <p>{props.Paragraph4}</p>
-//                         </div>
+                        <div className="benefitsCard">
+                            <h3><TbAntennaBars5 className="fawallet " size={40}/>{props.Heading4}</h3>
+                            <p>{props.Paragraph4}</p>
+                        </div>
 
 
-//                     </div>
-//                 </div>
+                    </div>
+                </div>
 
-//             </div>
-//         </>
-//   );
-// }
+            </div>
+        </>
+  );
+}
 
-// export default personalBenefits; 
+export default personalBenefits; 
